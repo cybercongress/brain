f97b4aa82a565ff78215a042a97bd034aecbd4e1
dimakorzhovnik <korzhovnik.dima@gmail.com>
2019-06-25 22:00:53 +0300

    redesign

--- imports/ui/voting-power/ThirtyFour.jsx
+++ imports/ui/voting-power/ThirtyFour.jsx
@@ -37,12 +37,12 @@ export default class ThirtyFour extends Component{
                                 this.props.stats.numBottomSixtySix
                             ],
                             backgroundColor: [
-                                '#bd081c',
-                                '#ff63c0'
+                                '#3ab793',
+                                '#1976d2'
                             ],
                             hoverBackgroundColor: [
-                                '#bd081c',
-                                '#ff63c0'
+                                '#3ab793',
+                                '#1976d2'
                             ]
                         }
                     ]
